db0126ce693512e1cd504825a3b93b116e89f13c
hemant-wedig <hemant.kumar@wedigtech.com>
2019-11-15 00:07:59 +0530

    first Setup
    first Setup implimentation

---
+++ newCrud/app/helpers/lib/first.helper.js
@@ -0,0 +1,13 @@
+module.exports = {
+    testing: function (req) {
+
+        return new Promise(function (resolve, reject) {
+            /*var response = {};
+            response['authCode'] = error_code;
+            response['status'] = failure_status_value;
+            response['msg'] = email_is_already_exits;
+            resolve(response);*/
+            console.log("Hello Testing")
+        });
+    },
+}
