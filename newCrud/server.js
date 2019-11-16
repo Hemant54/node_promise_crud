5697a52d8beba2d02f8f7183cd24b161021863f6
hemant-wedig <hemant.kumar@wedigtech.com>
2019-11-17 00:54:52 +0530

    Added Log Mantainer
    Added Log Mantainer

--- newCrud/server.js
+++ newCrud/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var loggers = require('express-logger');
+var morgan = require('morgan')
+
 // create express app
 const app = express();
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -7,8 +10,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 // define a simple route
+app.use(loggers({ path: "./logfile.txt" }));
+morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+})
+
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application."});
+    res.json({"message": "Welcome to node_promise_crud  application."});
 });
 require('./app/routes/first.routes')(app);
 
@@ -17,9 +31,9 @@ global.error_code = "100";
 global.success_status_value = true;
 global.failure_status_value = false;
         
-
-
 // listen for requests
 app.listen(3000, () => {
     console.log("Server is listening on http://localhost:3000/");
 });
+
+module.exports = app;
