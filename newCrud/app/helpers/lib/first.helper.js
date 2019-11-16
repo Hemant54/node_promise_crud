c6539443618cb64439b5eeed5f7de45c9ddaffdd
hemant-wedig <hemant.kumar@wedigtech.com>
2019-11-16 18:48:40 +0530

    router setup
    router setup successfully

--- newCrud/app/helpers/lib/first.helper.js
+++ newCrud/app/helpers/lib/first.helper.js
@@ -1,13 +1,22 @@
 module.exports = {
     testing: function (req) {
-        console.log("testing helper")
+        //console.log("testing helper")
         return new Promise(function (resolve, reject) {
-            /*var response = {};
-            response['authCode'] = error_code;
-            response['status'] = failure_status_value;
-            response['msg'] = email_is_already_exits;
-            resolve(response);*/
-            console.log("Hello Testing")
+            var user_name = req.body.username ? req.body.username : "";
+            if(user_name){
+                var response = {};
+                response['authCode'] = 200;
+                response['status'] = "ok";
+                response['msg'] = req.body;
+                resolve(response);
+            }else{
+                response['authCode'] = 400;
+                response['status'] = "Error";
+                response['msg'] = "Empty user_name";
+                resolve(response);
+            }
+            //resolve("Hello Testing")
+            //console.log("Hello Testing")
         });
     },
 }
--- newCrud/server.js
+++ newCrud/server.js
@@ -1,27 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Promise = require('promise');
-var jwt = require('jsonwebtoken');
-var _ = require('underscore');
-var cors = require('cors');
-const logger = require('morgan');
-var app = express();
-var moment = require('moment-timezone');
-//moment.tz.setDefault('Asia/Kolkata');
-moment.tz(Date.now(), "Asia/Kolkata");
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(logger('dev'));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
-require('./app/routes/users.routes')(app);
-app.set('port', process.env.PORT || 3003);
-var server = app.listen(app.get('port'), function () {
-    console.log('Server started, listening on port ' + app.get('port'));
-}, "0.0.0.0");
-server.timeout = 100000000;
-
-var loggers = require('express-logger');
-app.use(loggers({ path: "./logfile.txt" }));
-module.exports = app;
+// create express app
+const app = express();
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application."});
+});
+require('./app/routes/first.routes')(app);
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on http://localhost:3000/");
+});
